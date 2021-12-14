d3cb4b27da8fa71b9c7dcebd1bae50afc628e9a8
aldic9905 <61223424+aldic9905@users.noreply.github.com>
2021-12-14 13:04:46 +0900

    Update upbit.py

--- upbit.py
+++ upbit.py
@@ -10,14 +10,6 @@ def bot_chat(text):
     chat_id = 2129243345
     bot.sendMessage(chat_id=chat_id, text=text)
 
-def get_target_price(t,k): 
-    df = pyupbit.get_ohlcv(t, interval="minute10", count=2)
-    range = df.iloc[0]['high'] - df.iloc[0]['low']
-    return df.iloc[1]['open'] + range*k
-
-def get_current_price(t):
-    return pyupbit.get_orderbook(ticker=t)["orderbook_units"][0]["ask_price"]
-
 def rsi(ohlc: pandas.DataFrame, period: int = 14):
     delta = ohlc['close'].diff()
     ups, downs = delta.copy(), delta.copy()
@@ -30,52 +22,41 @@ def rsi(ohlc: pandas.DataFrame, period: int = 14):
 
     return pandas.Series(100-(100/(1+rs)), name = "rsi")
 
-
-
-
 access = "yI8stT3XVWVxqlCn4SAuq1aJGqxPBL89oa8QDr9D"          
 secret = "UEadUBTIqkz4UfV41alibKxbxVwoojfgYnWa9SvP"          
 upbit = pyupbit.Upbit(access, secret)
-tick = "KRW-ETH"
-
-btc = upbit.get_balance(tick)  
-krw = upbit.get_balance("KRW")
-buy_flag = False
-sell_flag = False
-
+tick = ["KRW-ETH", "KRW-BTC", "KRW-XRP", "KRW-SAND","KRW-MATIC"]
 bot_chat("자동매매 시작")
 print("Autotrade Start")
 while True:
     try:
         now = datetime.now()
-        btc = upbit.get_balance(tick)  
         krw = upbit.get_balance("KRW")
-        data = pyupbit.get_ohlcv(ticker=tick, interval="minute10")
-        now_rsi = rsi(data,14).iloc[-1]
-
-        if now_rsi <= 28 :
-            buy_flag = True
-        elif now_rsi >= 30 and buy_flag == True :
-            if krw > 5000:
-                upbit.buy_market_order(tick, krw*0.9995)
-                buy_flag = False
-                while True:
-                    if(upbit.get_order(tick)):
-                        continue
-                    else:
-                        bot_chat("매수완료")
-                        print("buy")
-                        break
-        elif now_rsi >= 50 :
-            if btc > 0.001:
-                upbit.sell_market_order(tick, btc*0.9995)
-                while True:
-                    if(upbit.get_order(tick)):
-                        continue
-                    else:
-                        bot_chat("매도완료")
-                        print("sell")
-                        break
+        
+        for i in range(len(tick)):
+            btc = upbit.get_balance(tick[i])
+            data = pyupbit.get_ohlcv(ticker=tick[i], interval="minute60")
+            now_rsi = rsi(data,14).iloc[-1]
+            if now_rsi <= 28 :
+                if krw > 5000:
+                    upbit.buy_market_order(tick[i], krw*0.9995)
+                    while True:
+                        if(upbit.get_order(tick[i])):
+                            continue
+                        else:
+                            bot_chat(tick[i]+"매수완료")
+                            print("buy")
+                            break
+            elif now_rsi >= 60 :
+                if btc:
+                    upbit.sell_market_order(tick[i], btc)
+                    while True:
+                        if(upbit.get_order(tick[i])):
+                            continue
+                        else:
+                            bot_chat(tick[i]+"매도완료")
+                            print("sell")
+                            break
         time.sleep(1)
     except Exception as e:
         print(e)
