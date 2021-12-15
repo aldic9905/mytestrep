d8cb1fce64942a862fda9d23b58e04225cf8e9ec
aldic9905 <61223424+aldic9905@users.noreply.github.com>
2021-12-15 17:30:33 +0900

    Update test.py

--- test.py
+++ test.py
@@ -25,24 +25,23 @@ def rsi(ohlc: pandas.DataFrame, period: int = 14):
 access = "yI8stT3XVWVxqlCn4SAuq1aJGqxPBL89oa8QDr9D"          
 secret = "UEadUBTIqkz4UfV41alibKxbxVwoojfgYnWa9SvP"          
 upbit = pyupbit.Upbit(access, secret)
-tick = ["KRW-ETH", "KRW-BTC", "KRW-XRP", "KRW-SAND","KRW-DOGE"]
-pre_rsi = [-1,-1,-1,-1,-1]
-buy_flag = [-1,-1,-1,-1,-1]
-sell_flag = [-1,-1,-1,-1,-1]
+tick = ["KRW-ETH","KRW-BTC","KRW-SAND","KRW-MATIC"]
+max_rsi = [0,0,0,0]
+min_rsi = [100,100,100,100,100]
 bot_chat("자동매매 시작")
 print("Autotrade Start")
 while True:
     try:
-        now = datetime.now()
-        krw = upbit.get_balance("KRW")
         
         for i in range(len(tick)):
+            now = datetime.now()
+            krw = upbit.get_balance("KRW")
             btc = upbit.get_balance(tick[i])
-            data = pyupbit.get_ohlcv(ticker=tick[i], interval="minute30")
+            data = pyupbit.get_ohlcv(ticker=tick[i], interval="minute3")
             now_rsi = rsi(data,14).iloc[-1]
-            if now_rsi > pre_rsi and pre_rsi != -1:
-                sell_flag[i] = 0
-            elif now_rsi < pre_rsi and sell_flag==0 and pre_rsi != -1:
+            if max_rsi[i] < now_rsi:
+                max_rsi[i] = now_rsi
+            elif max_rsi[i] > now_rsi:
                 if btc:
                     upbit.sell_market_order(tick[i], btc)
                     while True:
@@ -52,22 +51,19 @@ while True:
                             bot_chat(tick[i]+" 매도완료")
                             print("sell")
                             break
-                sell_flag[i] =-1
-            if now_rsi < pre_rsi and pre_rsi != -1:
-                buy_flag[i] = 1
-            elif now_rsi > pre_rsi and buy_flag==1 and pre_rsi != -1:
-                if krw > 5000:
-                    upbit.buy_market_order(tick[i], krw*0.9995)
-                    while True:
-                        if(upbit.get_order(tick[i])):
-                            continue
-                        else:
-                            bot_chat(tick[i]+" 매수완료")
-                            print("buy")
-                            break
-                buy_flag[i] =-1
-        pre_rsi = rsi(data,14).iloc[-1]
-        time.sleep(1)
+                if now_rsi < min_rsi[i]:
+                    min_rsi[i] = now_rsi
+                elif now_rsi > min_rsi[i]:
+                    if krw > 5000:
+                        upbit.buy_market_order(tick[i], krw*0.9995)
+                        while True:
+                            if(upbit.get_order(tick[i])):
+                                continue
+                            else:
+                                bot_chat(tick[i]+" 매수완료")
+                                print("buy")
+                                break
+        time.sleep(180)
     except Exception as e:
         print(e)
         time.sleep(1)
