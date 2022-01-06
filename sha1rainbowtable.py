077212f3e2055d436d8e15e1cd10ce3ed9ede957
aldic9905 <61223424+aldic9905@users.noreply.github.com>
2022-01-06 10:44:31 +0900

    Add files via upload

---
+++ sha1rainbowtable.py
@@ -0,0 +1,17 @@
+import hashlib
+import telegram
+
+def bot_chat(text):   
+    bot = telegram.Bot(token='5099900246:AAGdOEWXA4XJ8PBGx234H9YiBOc9v4TcJsE')
+    chat_id = 2129243345
+    bot.sendMessage(chat_id=chat_id, text=text)
+
+a = 'd142afc1ac4a918f459c0a94ca9265ee43841e96'
+
+for i in range(10000000,99999999+1):
+    pw = hashlib.sha1((str(i)+'salt_for_you').encode()).hexdigest()
+    if pw == a :
+        bot_chat(str(i)+'salt_for_you')
+        break
+    print('no'+str(i))
+ 
